c70a855e87a4c5d445b4d2d0629170302c3b8108
KostyaTretyak <ktretiak.in.ua@gmail.com>
2020-05-09 14:02:03 +0300

    Removed @ng-stack/api-mock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { ApiMockModule } from '@ng-stack/api-mock';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FakeApiService } from './api-mock.service';
 
 @NgModule({
   declarations: [
@@ -15,8 +12,7 @@ import { FakeApiService } from './api-mock.service';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    HttpClientModule,
-    ApiMockModule.forRoot(FakeApiService)
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
